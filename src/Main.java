7f0f6c30cfe87c01558a39c258cf51b64f4c91dc
alfer <alfer.natalie@gmail.com>
2026-07-23 16:50:07 +0300

    mouse difficulty levels added

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import java.util.Random;
 import java.util.ArrayList;
 
 
+
 class GameWindow{
 
     //объявление переменной, которая позже будет хранить ссылку на объект
@@ -75,7 +76,7 @@ class GamePanel extends JPanel{
     // создаем объект класса Enemy, задаем в параметрах координаты x и y
     // 500      - время реакции в миллисекундах
     // 80       - точность движения (%)
-    private Enemy mouse = new Enemy(700, 400, 500, 95);
+    private Enemy mouse = new Enemy(700, 400, 500, 20);
 
 //    private static final int SEGMENT_DISTANCE = 70;
 
@@ -353,139 +354,231 @@ class GamePanel extends JPanel{
 }
 
 
-class Enemy{
 
-    // Текущая позиция мыши
+// мышь получает информацию не постоянно - reactionTime
+// запоминание последнего местонахождения змейки
+// Разделение кадров и принятия решений - Game loop 60 FPS и AI thinking через makeDecision();
+// ошибка это состояние, которое длится определенное время
+// Вероятность ошибки - mistakeChance
+// Два режима поведения мыши - NORMAL (moveToTarget()) и MISTAKE (mistakeDirection)
+
+
+// position - где мышь сейчас
+// target - последняя известная позиция змейки
+// reactionTime - как часто получает информацию
+// mistakeChance - вероятность ошибиться
+// nextDecisionTime - когда можно снова подумать
+// mistakeDirection - куда ошибочно идёт
+// mistakeEndTime - когда закончится ошибка
+
+
+
+// Лёгкая мышь
+// reactionTime = 1200;
+// mistakeChance = 40;
+// mistakeDuration = 1200;
+// speed = 2;
+
+
+// Средняя мышь
+//reactionTime = 700;
+//mistakeChance = 20;
+//mistakeDuration = 700;
+//speed = 3;
+
+
+// Сложная мышь
+//reactionTime = 200;
+//mistakeChance = 5;
+//mistakeDuration = 300;
+//speed = 5;
+
+
+
+
+class Enemy {
+
+
+    // Позиция
     private Point position;
 
-    // Это НЕ настоящая позиция змейки.
-    // Это координата, которую мышь запомнила в последний момент обновления информации.
+
+    // Последняя известная позиция змейки
     private Point target;
 
-    // скорость мыши
+    // Движение
     private int speed = 3;
-
-    // размер круга мыши
     private static final int SIZE = 50;
 
-    // Через сколько миллисекунд мышь получает новую информацию
+    // Как часто мышь получает новую информацию
     private int reactionTime;
 
-    // Насколько хорошо мышь принимает решения.
-    private int accuracy;
-
-    // Храним момент последнего получения информации
+    // Когда последний раз мышь получила координаты
     private long lastUpdateTime;
+
+
+    // AI мыши
+
+
+    // Шанс ошибки при принятии решения
+    private int mistakeChance;
+
+
+    // Когда мышь снова будет принимать решение
+    private long nextDecisionTime;
+
+
+    // Состояние ошибки
+    // Направление ошибочного движения
+    private Point mistakeDirection;
+
+    // Когда закончится ошибка
+    private long mistakeEndTime;
+
+    // Сколько длится ошибка
+    private int mistakeDuration = 800;
 
     private Random random = new Random();
 
-    private Point mistakeDirection;
 
-    private long mistakeEndTime;
+    // Конструктор
+    public Enemy(
+            int x,
+            int y,
+            int reactionTime,
+            int mistakeChance
+    ){
 
-    private int mistakeDuration = 250;
+        position = new Point(x,y);
+        target = new Point(x,y);
 
-
-
-    public Enemy(int x, int y, int reactionTime, int accuracy){
-
-        position = new Point(x, y);
-        target = new Point(x, y);
         this.reactionTime = reactionTime;
-        this.accuracy = accuracy;
-
-        lastUpdateTime = 0;
+        this.mistakeChance = mistakeChance;
 
         mistakeDirection = new Point(0,0);
+        lastUpdateTime = 0;
+        nextDecisionTime = 0;
+        mistakeEndTime = 0;
+
+
+
     }
 
+
+
+    // Получение информации о змейке
     public void updateTarget(Point snakeHead){
-//        target = new Point(snakeHead);
 
         long currentTime = System.currentTimeMillis();
 
         if (currentTime - lastUpdateTime >= reactionTime){
-            target = new Point(snakeHead);
+
+            target = new Point(
+                    snakeHead
+            );
 
             lastUpdateTime = currentTime;
+
         }
 
     }
 
+
+    // Основное движение
     public void move(){
 
         long currentTime = System.currentTimeMillis();
 
-        if (currentTime < mistakeEndTime){
+
+        /*
+        Состояние ошибки.
+        Если мышь уже ошиблась,
+        она НЕ принимает новые решения.
+        Она просто выполняет старое решение.
+        */
+
+
+        if(currentTime < mistakeEndTime){
 
             position.x += mistakeDirection.x * speed;
             position.y += mistakeDirection.y * speed;
 
             return;
+
         }
 
 
-        boolean mistake = random.nextInt(100) >= accuracy;
+        /*
+        Ошибка закончилась.
+        Проверяем:
+        может ли мышь принять новое решение?
+        */
 
-        if (mistake){
-            createMistake();
-            return;
+
+        if (currentTime >= nextDecisionTime){
+            makeDecision();
         }
 
+
+        // Если решения нет -
+        // продолжаем нормальное движение
         moveToTarget();
-
-//        if (position.x < target.x){
-//            position.x += speed;
-//        }
-//
-//        if (position.x > target.x){
-//            position.x -= speed;
-//        }
-//
-//        if (position.y < target.y){
-//            position.y += speed;
-//        }
-//
-//        if (position.y > target.y){
-//            position.y -= speed;
-//        }
 
     }
 
 
+    // Принятие решения
+    private void makeDecision(){
+
+        long currentTime = System.currentTimeMillis();
+
+
+        // запрещаем принимать решение
+        // слишком часто
+
+        nextDecisionTime = currentTime + 500;
+
+        int chance = random.nextInt(100);
+
+        if (chance < mistakeChance){
+            createMistake();
+
+        }
+    }
+
+
+    // Создание ошибки
     private void createMistake(){
 
         int direction = random.nextInt(4);
 
+
         switch (direction){
 
             case 0:
-
                 mistakeDirection = new Point(1,0);
                 break;
 
             case 1:
-
                 mistakeDirection = new Point(-1,0);
                 break;
 
             case 2:
-
                 mistakeDirection = new Point(0,1);
                 break;
 
             case 3:
-
                 mistakeDirection = new Point(0,-1);
                 break;
-
         }
 
         mistakeEndTime = System.currentTimeMillis() + mistakeDuration;
 
     }
 
+    // Нормальное движение к цели
     private void moveToTarget(){
+
 
         if (position.x < target.x){
             position.x += speed;
@@ -494,6 +587,7 @@ class Enemy{
         if (position.x > target.x){
             position.x -= speed;
         }
+
 
         if (position.y < target.y){
             position.y += speed;
@@ -506,45 +600,23 @@ class Enemy{
     }
 
 
-    private void randomMove(){
-
-        int direction = random.nextInt(4);
-
-        switch (direction){
-
-            case 0:
-                position.x += speed;
-                break;
-
-            case 1:
-                position.x -= speed;
-
-
-            case 2:
-                position.y += speed;
-                break;
-
-            case 3:
-                position.y -= speed;
-        }
-
-
-    }
-
+    // Для рисования
     public int getX(){
         return position.x;
     }
+
 
     public int getY(){
         return position.y;
     }
 
+
     public int getSize(){
         return SIZE;
     }
 
-
 }
+
 
 
 
